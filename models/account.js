702cef3b952c5d828972e0871890be2be59e3980
matteo <matteo.tomasi@ynov.com>
2021-10-27 08:04:38 +0100

    wesh

---
+++ models/account.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Account extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.Account.belongsTo(models.Rank, {
+        foreignKey: {
+          allowNull: false,
+        }
+      })
+
+    }
+  };
+  Account.init({
+    id: DataTypes.BIGINT,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    id_rank: DataTypes.BIGINT
+  }, {
+    sequelize,
+    modelName: 'Account',
+  });
+  return Account;
+};
---
+++ models/post.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.Post.belongsTo(models.Category, {
+        foreignKey: {
+          allowNull: false,
+        }
+      })
+    }
+  };
+  Post.init({
+    id: DataTypes.BIGINT,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    date_post: DataTypes.DATE,
+    id_category: DataTypes.BIGINT
+  }, {
+    sequelize,
+    modelName: 'Post',
+  });
+  return Post;
+};
