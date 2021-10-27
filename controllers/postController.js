ecdcb434babee5e3153d1b508150e4242da7781e
Bastien <bastien.brunet.pro@gmail.com>
2021-10-27 15:49:24 +0200

    Récuperation d'un post via son id

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const db = require('../models/index');
+const Console = require("console");
 const posts = require("../models/post")(db.sequelize, db.Sequelize.DataTypes);
 
 router.post('/', async (req, res) => {
-    console.log(req.body)
+    console.log("/")
 
     const title = req.body.title
     const content = req.body.content
@@ -27,9 +28,20 @@ router.post('/', async (req, res) => {
 })
 
 router.get('/',async (req, res) => {
+    Console.log("/post")
     let allPosts = await posts.findAll()
 
     return res.status(200).send(allPosts)
+})
+
+router.get('/:postId',async (req, res) => {
+    Console.log("/post/",req.params.postId)
+    let singlePost = await posts.findByPk(req.params.postId)
+
+    if (!singlePost)
+        return res.status(400).send("Id out of range")
+
+    return res.status(200).send(singlePost)
 })
 
 module.exports = router;
